12b5427ccae6968a59fcfe3696a2c36518f19a1c
Scott Nelson <scott@scttnlsn.com>
2013-02-27 15:34:02 -0500

    Clean up example

--- examples/basic/server.js
+++ examples/basic/server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var store = require('./store');
 
 var app = express();
 var server = app.listen(process.env.PORT || 3000);
@@ -9,55 +10,8 @@ app.use(express.static(__dirname + '/../..'));
 var io = require('socket.io').listen(server);
 var data = require('../../lib/index')(io);
 
-// CRUD
-
-var store = {};
-var id = 1;
 var messages = data.bucket('messages');
-
-messages.use('create', function(req, res, next) {
-    var message = req.data;
-    message.id = id++;
-    store[message.id] = message;
-
-    res.send(message);
-});
-
-messages.use('update', function(req, res, next) {
-    var id = req.data.id;
-    var message = store[id];
-
-    if (!message) return res.error(new Error('No such message'));
-
-    for (var key in req.data) {
-        message[key] = req.data[key];
-    }
-
-    store[id] = message;
-    res.send(message);
-});
-
-messages.use('delete', function(req, res, next) {
-    var id = req.data.id;
-    var message = store[id];
-    delete store[id];
-    res.send(message);
-});
-
-messages.use('list', function(req, res, next) {
-    var messages = [];
-    for (var id in store) {
-        messages.push(store[id]);
-    }
-    res.send(messages);
-});
-
-messages.use('read', function(req, res, next) {
-    var id = req.data.id;
-    res.send(store[id]);
-});
-
-// Notify
+messages.use(store());
 
 messages.on('sync', function(result, req) {
     // TODO: This needs to be cleaner
---
+++ examples/basic/store.js
@@ -0,0 +1,38 @@
+module.exports = function() {
+    var models = {};
+    var id = 1;
+    
+    return function(req, res, next) {
+        var crud = {
+            create: function() {
+                var model = req.data;
+                model.id = id++;
+                models[model.id] = model;
+                res.send(model);
+            },
+            
+            read: function() {
+                res.send(models[req.data.id]);
+            },
+
+            list: function() {
+                var values = [];
+                for (var id in models) values.push(models[id]);
+                res.send(values);
+            },
+            
+            update: function() {
+                models[req.data.id] = req.data;
+                res.send(req.data);
+            },
+            
+            delete: function() {
+                delete models[req.data.id];
+                res.send(req.data);
+            }
+        };
+        
+        if (!crud[req.action]) return next(new Error('Unsuppored action: ' + req.action));
+        crud[req.action]();
+    }
+};
