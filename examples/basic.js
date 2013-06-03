c86d9751237f3613ef3667f2924b9bac3a08ddfe
Scott Nelson <scott@scttnlsn.com>
2013-06-03 14:07:04 -0400

    Rearrange examples

--- examples/basic.js
+++ examples/basic.js
@@ -4,10 +4,10 @@ var store = require('./store');
 var app = express();
 var server = app.listen(process.env.PORT || 3000);
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/public'));
 
 var io = require('socket.io').listen(server);
-var data = require('../../lib/index')(io);
+var data = require('../lib/index')(io);
 
 var messages = data.resource('messages');
 messages.use(store());
